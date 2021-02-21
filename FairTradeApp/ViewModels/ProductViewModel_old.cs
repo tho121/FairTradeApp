e654407d4d523893902159e9cc9eba8df56bb70c
Tony Ho <tho121@hotmail.com>
2021-02-21 02:13:54 -0800

    duplicated files to facilitate rebasing

---
+++ FairTradeApp/ViewModels/ProductViewModel_old.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace FairTradeApp.ViewModels
+{
+	public class ProductViewModel_old : BaseViewModel
+	{
+		public ProductViewModel_old()
+		{
+			Product = "Banana";
+			Title = "Fair Trade App!!";
+			Country = "Panama";
+			Image = "banana.png";
+			Premium = "Premium: 15 %";
+			OpenWebCommand = new Command(async () => await Shell.Current.Navigation.PushModalAsync(new Views.Premium_calc())) ;
+		}
+
+		public ICommand OpenWebCommand { get; }
+
+		string product = string.Empty;
+		public string Product
+		{
+			get { return product; }
+			set { SetProperty(ref product, value); }
+		}
+
+		string country = string.Empty;
+		public string Country
+		{
+			get { return country; }
+			set { SetProperty(ref country, value); }
+		}
+
+		string premium = string.Empty;
+		public string Premium
+		{
+			get { return premium; }
+			set { SetProperty(ref premium, value); }
+		}
+
+		string image = string.Empty;
+		public string Image
+		{
+			get { return image; }
+			set { SetProperty(ref image, value); }
+		}
+
+	}
+}
---
+++ FairTradeApp/Views/ProductPage_old.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FairTradeApp.Views
+{
+	public partial class ProductPage_old : ContentPage
+	{
+		public ProductPage_old()
+		{
+			InitializeComponent();
+		}
+	}
+}
