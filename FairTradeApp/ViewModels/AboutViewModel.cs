58daef864ccb147ac3827d157e5d114c1ede4765
Rustem Ismagilov <Rustem2001@gmail.com>
2021-02-21 00:09:46 -0800

    Product

--- FairTradeApp/ViewModels/AboutViewModel.cs
+++ FairTradeApp/ViewModels/AboutViewModel.cs
@@ -9,10 +9,43 @@ namespace FairTradeApp.ViewModels
 	{
 		public AboutViewModel()
 		{
+			Product = "Banana";
 			Title = "Fair Trade App!!";
+			Country = "Panama";
+			Image = "banana.png";
+			Premium = "Premium: 15 %";
 			OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamarin-quickstart"));
 		}
 
 		public ICommand OpenWebCommand { get; }
+
+		string product = string.Empty;
+		public string Product
+		{
+			get { return product; }
+			set { SetProperty(ref product, value); }
+		}
+
+		string country = string.Empty;
+		public string Country
+		{
+			get { return country; }
+			set { SetProperty(ref country, value); }
+		}
+
+		string premium = string.Empty;
+		public string Premium
+		{
+			get { return premium; }
+			set { SetProperty(ref premium, value); }
+		}
+
+		string image = string.Empty;
+		public string Image
+		{
+			get { return image; }
+			set { SetProperty(ref image, value); }
+		}
+
 	}
 }
---
+++ FairTradeApp/ViewModels/Premium_calc.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace FairTradeApp.ViewModels
+{
+	public class Premium_calc : BaseViewModel
+	{
+		int weight;
+		public Premium_calc()
+		{
+			Weight = 0;
+
+			TextChange = new Command(() =>
+				{
+					//do calculation
+					
+				}
+			);
+		}
+
+		
+		public int Weight
+		{
+			get { return weight; }
+			set { SetProperty(ref weight, value); }
+		}
+
+		int premium_res;
+		public int Premium_res
+		{
+			get { return premium_res; }
+			set { SetProperty(ref premium_res, value); }
+		}
+
+		//public void TextChange(object sender, TextChangedEventArgs e)
+		//{
+		//	this.textChangedLabel.Text = $"Text changed from {e.OldTextValue} to {e.NewTextValue}";
+		//}
+
+		public ICommand TextChange { get; }
+	}
+}
